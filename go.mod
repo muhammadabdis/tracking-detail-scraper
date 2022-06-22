12d192bb7c2068f201f0d73395a024ec5f82c1a8
Muhammad Abdis <abdissalim0@gmail.com>
2022-06-23 01:53:43 +0700

    Output API response to json

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/tracking-detail-scraper
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
--- main.go
+++ main.go
@@ -3,39 +3,40 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
+	"regexp"
+	"strings"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
 )
 
 var PORT int = 3000
 
+var PAGE_URL string = "https://gist.githubusercontent.com/nubors/eecf5b8dc838d4e6cc9de9f7b5db236f/raw/d34e1823906d3ab36ccc2e687fcafedf3eacfac9/jne-awb.html"
+
 type Track struct {
-	Description string `json:"description"`
-	CreatedAt   string `json:"createdAt"`
-	Formatted   struct {
-		CreatedAt string `json:"createdAt"`
-	} `json:"formatted"`
+	Description string            `json:"description"`
+	CreatedAt   string            `json:"createdAt"`
+	Formatted   map[string]string `json:"formatted"`
+}
+
+type TrackHistory struct {
+	ReceivedBy string   `json:"receivedBy"`
+	Histories  []*Track `json:"histories"`
 }
 
 type TrackingResponse struct {
-	Status struct {
-		Code    string `json:"code"`
-		Message string `json:"message"`
-	} `json:"status"`
-	Data struct {
-		ReceivedBy string   `json:"receivedBy"`
-		Histories  []*Track `json:"histories"`
-	} `json:"data"`
+	Status map[string]string `json:"status"`
+	Data   *TrackHistory     `json:"data"`
 }
 
-var baseData TrackingResponse
+var data TrackingResponse
 
 func main() {
-	initTestData()
+	scrapeDataTo(&data)
 
 	router := mux.NewRouter()
 
@@ -48,17 +49,6 @@ func main() {
 	)
 }
 
-func initTestData() {
-	file, _ := os.Open("example.json")
-
-	log.Println("Successfully read json data")
-
-	defer file.Close()
-
-	b, _ := io.ReadAll(file)
-	json.Unmarshal(b, &baseData)
-}
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Tracking Detail Scraper by Muhammad Abdis")
 }
@@ -66,5 +56,59 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func trackingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(baseData)
+	json.NewEncoder(w).Encode(data)
+}
+
+func scrapeDataTo(data *TrackingResponse) {
+	res, err := http.Get(PAGE_URL)
+
+	if err != nil {
+		log.Println("Cannot reach requested URL")
+	}
+
+	defer res.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	receivedBy := doc.Find(".tracking:last-child tbody tr:last-child td:nth-child(2)").Text()
+	re := regexp.MustCompile(`\[(.*?)\s+\|`)
+	receivedBy = re.FindStringSubmatch(receivedBy)[1]
+
+	histories := make([]*Track, 0)
+	doc.Find(".tracking:last-child tbody tr").Each(func(i int, s *goquery.Selection) {
+		createdAt, _ := time.Parse("02-01-2006 15:04 MST", s.Find("td:first-child").Text()+" WIB")
+
+		monthReplacer := strings.NewReplacer(
+			"January", "Januari",
+			"February", "Februari",
+			"March", "Maret",
+			"May", "Mei",
+			"April", "April",
+			"June", "Juni",
+			"July", "Juli",
+			"August", "Agustus",
+			"September", "September",
+			"October", "Oktober",
+			"November", "November",
+			"December", "Desember",
+		)
+
+		histories = append(histories, &Track{
+			Description: s.Find("td:nth-child(2)").Text(),
+			CreatedAt:   createdAt.Format(time.RFC3339),
+			Formatted: map[string]string{
+				"createdAt": monthReplacer.Replace(
+					createdAt.Format("02 January 2006, 15:04 MST"),
+				),
+			},
+		})
+	})
+
+	data.Status = map[string]string{
+		"code":    "060101",
+		"message": "Delivery tracking detail fetched successfully",
+	}
+	data.Data = &TrackHistory{
+		ReceivedBy: receivedBy,
+		Histories:  histories,
+	}
 }
